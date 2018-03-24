05a2434dfeb0c520f2665c3bdc3e358f8d7723da
ryankruse <rpkruse@ksu.edu>
2018-03-24 10:42:04 -0500

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DownloadComponent } from './download/download.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DownloadComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true })    
   ],
   providers: [],
   bootstrap: [AppComponent]
